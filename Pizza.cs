3015792b15f5d1d37d08f5bec74ef28293fb0a4e
abc <abc@gmail.com>
2024-02-24 15:53:58 +0530

    first prj

---
+++ Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FebSec4Prj
+{
+    public class Pizza
+    {
+        public string Authenticate(string CustomerName, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(CustomerName) || string.IsNullOrEmpty(password))
+            {
+                msg = "user name and password required ";
+            }
+            else
+            {
+                if ((CustomerName == "admin") && (password == "admin123@"))
+                {
+                    msg = "Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentcation fail";
+                }
+            }
+            return "provide user details";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FebSec4Prj;
+
+Console.WriteLine("WELCOME TO JOE'S PIZZA SHOAP!");
+Pizza p=new Pizza();
+Console.WriteLine(p.Authenticate("admin", "admin123@"));
